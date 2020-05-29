e27c86dbfff96e74e1c6bdd6ed59ecd1eeb164a8
manak1 <mana23happy@hotmail.co.jp>
2020-05-29 12:58:21 +0900

    chore: update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,14 +31,9 @@ export default {
   /*
    ** Nuxt.js dev-modules
    */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
-  ],
+  buildModules: ['@nuxtjs/tailwindcss'],
+
+  devModules: ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module'],
   /*
    ** Nuxt.js modules
    */
