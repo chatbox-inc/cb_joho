faf845332a21477653e5f760ce48ccb0f9c46b25
manak1 <mana23happy@hotmail.co.jp>
2020-05-29 17:05:24 +0900

    feature: play audio

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,53 @@
 <template>
-  <div class="container mx-auto">
-    <h1>Hello World</h1>
+  <div class="container mx-auto pt-12">
+    <h2 class="text-sm bg-red-400 inline-block p-2 text-white rounded">
+      現在時刻
+    </h2>
+    <time
+      class="text-huge leading-none select-none font-bold mt-3 block text-shadow font-mono"
+    >
+      {{ currenTime }}
+    </time>
+    <button
+      class="bg-blue-400 p-5 rounded text-white text-lg hover:bg-blue-600"
+      @click.prevent="playSound"
+    >
+      test sound
+    </button>
   </div>
 </template>
 
 <script>
+import sound from '@/assets/sound/test.mp3'
 export default {
-  components: {}
+  components: {},
+  data() {
+    return {
+      currenTime: null
+    }
+  },
+  mounted() {
+    this.currenTime = this.setDate()
+    setInterval(() => this.setDate(), 1000)
+  },
+  methods: {
+    setDate() {
+      this.currenTime = this.$dayjs().format('HH:mm:ss')
+    },
+    playSound() {
+      const audio = new Audio(sound)
+      audio.play()
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.text-shadow {
+  text-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+}
+
+.text-huge {
+  font-size: 16rem;
+}
+</style>
