3b3b2924f83a442f26f1d364210fa8dad92cd293
Yoshi <yoshis1057@gmail.com>
2020-06-17 14:18:57 +0900

    update

--- service/PollyService.js
+++ service/PollyService.js
@@ -2,15 +2,16 @@ const fs = require('fs')
 const aws = require('aws-sdk')
 require('dotenv').config({ path: '../.env' })
 
+// 環境変数(.env)を設定する必要あり
 aws.config.update({
   credentials: new aws.Credentials(
     process.env.AWS_ACCESS_KEY_ID,
     process.env.AWS_SECRET_ACCESS_KEY
   ),
-  region: 'us-east-2'
+  region: process.env.AWS_DEFAULT_REGION
 })
 
-const polly = new aws.Polly({ region: 'us-east-2' })
+const polly = new aws.Polly({ region: process.env.AWS_DEFAULT_REGION })
 const params = {
   OutputFormat: 'mp3',
   Text: '12:55:55', // 出力される音声文字列
