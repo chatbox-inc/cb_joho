e227640b9e87376e0a1fdef5a1197f16d2805ff9
Yoshi <yoshis1057@gmail.com>
2020-07-15 13:57:22 +0900

    設定の保存

--- pages/index.vue
+++ pages/index.vue
@@ -74,6 +74,10 @@ export default {
   mounted() {
     this.currenTime = this.setTime()
     this.currentDate = this.setDate()
+    this.alertMode = localStorage.alertMode === 'true'
+    this.alertFrequency = localStorage.alertFrequency
+      ? parseInt(localStorage.alertFrequency, 10)
+      : 30
     setInterval(() => this.setTime(), 1000)
   },
   methods: {
@@ -112,10 +116,12 @@ export default {
       this.alertMode = true
       this.alertFrequency = alertFrequency
       this.slideSound()
+      this.persist()
     },
     offAlert() {
       this.alertMode = false
       this.slideSound()
+      this.persist()
     },
 
     checkAlert(currentTime) {
@@ -154,6 +160,10 @@ export default {
           this.isEvening = false
           break
       }
+    },
+    persist() {
+      localStorage.alertMode = this.alertMode
+      localStorage.alertFrequency = this.alertFrequency
     }
   }
 }
